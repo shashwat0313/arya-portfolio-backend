c2de32f11252c75859bbf281c3bcd9e26bf2a121
shashwat0313 <shashwat0313@yahoo.co.in>
2025-04-21 00:45:44 +0530

    auth, and get files from github repo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
-import authRouter from "./routes/authRouter.js"
-import initInterceptor from "./middlewares/initInterceptor.js";
+// import authRouter from "./routes/authRouter.js"
+import interceptor from "./middlewares/Interceptor.js";
+import routesBinder from "./routes/RoutesBinder.js";
+import cors from "cors"
 
 // Load env
 dotenv.config();
@@ -11,12 +13,19 @@ const app = express();
 const PORT = process.env.PORT;
 
 // json parsing for json body
-app.use(express.json())
+app.use(express.json());
 
-app.use(initInterceptor)
+// Enable CORS for localhost:5173
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
+
+// interceptor
+app.use(interceptor);
 
 // route bindings
-app.use("/auth", authRouter);
+routesBinder(app);
+// app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
---
+++ routes/RoutesBinder.js
@@ -0,0 +1,12 @@
+import authRouter from "./authRouter.js";
+import githubRouter from "./githubRouter.js";
+import { authenticateToken } from "../middlewares/auth.js";
+
+const routesBinder = (app) => {
+    app.use("/auth", authRouter);
+
+    // protected routes
+    app.use("/github-api", authenticateToken, githubRouter)
+}
+
+export default routesBinder;
